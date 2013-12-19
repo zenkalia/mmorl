fff8415e10118a79d189e1821e49e0cfc4669332
Ken Kendall <zenkalia@gmail.com>
2013-12-19 11:02:17 -0800

    :school: rails gee thang

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mmorl::Application.config.secret_key_base = '6d67ed246849b65b65e04774b5c7ae51b869cab99e40485940e648c01dbae3fb2999bcc6a918f60caf7b95b87a4935183f525eb5012928aea4813fe8f65667c6'
