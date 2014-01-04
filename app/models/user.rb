7e537ab56b3179953ac0a84290a6d60fa7671804
Mike Kendall <zenkalia@gmail.com>
2014-01-04 01:17:00 -0800

    :exclamation: kill my bad line of sight code

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,36 +38,6 @@ class User < ActiveRecord::Base
     1
   end
 
-  def can_see?(target_row, target_col)
-    target = Vector[target_row, target_col]
-    consider = trigger = Vector[self.row, self.col]
-    diff = target - consider
-    trigger_step = Vector[0.999999, 0.999999]
-    carry = Vector[0, 0]
-    step = diff.normalize.map(&:round)
-
-    if diff[0].abs > diff[1].abs
-      carry += Vector[0, diff[1] / diff[1].abs] if diff[1] != 0
-      trigger_step = Vector[diff[0].abs / diff[1].abs.to_f - 0.000001, trigger_step[1]]
-    else
-      carry += Vector[diff[0] / diff[0].abs, 0] if diff[0] != 0
-      trigger_step = Vector[trigger_step[0], diff[1].abs / diff[0].abs.to_f - 0.000001]
-    end
-
-    trigger += trigger_step / 2
-
-    while consider != target
-      return false if Fixture.where(row: consider[0], col: consider[1], solid: true).any?
-      if consider[0] > trigger[0] or consider[1] > trigger[1]
-        consider += carry
-        trigger += trigger_step
-      else
-        consider += step
-      end
-    end
-    return true
-  end
-
   private
   def not_standing_on_wall
     errors.add(:row, 'not an open cell') if self.room.get_fixture(self.row, self.col) == '#' or self.row < 1 or self.row > 20 or self.col < 1 or self.col > 80
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,27 +1,4 @@
 require 'spec_helper'
 
-# +-----
-# |   1
-# | @ #2
-# |   #<-3
-
 describe User do
-  let(:room) { Room.create() }
-  let(:user) { User.create(room: room, row: 2, col: 2) }
-  before do
-    Fixture.create(char: '#', row: 2, col: 4, solid: true)
-    Fixture.create(char: '#', row: 3, col: 4, solid: true)
-  end
-
-  describe :can_see? do
-    xit 'lets you see things you can see' do
-      user.can_see?(1,4).should be_true
-    end
-    xit 'blocks things behind walls' do
-      user.can_see?(2,5).should be_false
-    end
-    xit 'lets you see existing walls' do
-      user.can_see?(3,4).should be_true
-    end
-  end
 end
